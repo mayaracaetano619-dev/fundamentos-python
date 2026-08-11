05446a35488b017b3baf9ecb5a0fc1681b7b7340
43068048877 <mayaracaetano619@gmail.com>
2026-08-11 11:23:12 -0300

    nova atividade

--- estruturas_desicao/exemplos_if_else.py
+++ estruturas_desicao/exemplos_if_else.py
@@ -22,13 +22,24 @@ def aluno_aprovado():
 def login():
     e_mail = "mayaracaetano619@gmail.com"
     senha = "1234"
-
+    codigo_segreto = "#5678"
 
     email_input = input('Digite o seu e-mail: ')
     senha_input = input('Digite sua senha: ')
 
     if email_input == e_mail and senha_input == senha:
         print('Usuário logado!')
+        acessar_admin = input('Deseja acessar área administrativa? [S/N]')
+        if acessar_admin == 'S':
+            codigo_segreto_input = input('Digite o seu codigo secreto: ')
+            if codigo_segreto_input == codigo_segreto:
+                print('Acesso adm liberado!')
+            else:
+                print('Código secreto incorreto!')
+        elif acessar_admin == 'N':
+            print('Ok. Você acessou como usuário comum')
+        else:
+            print('Opção invalida!')
     else:
         print('E-mail ou senha incorreta!')
 
