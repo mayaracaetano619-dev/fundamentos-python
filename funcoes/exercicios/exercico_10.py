a7e70dea5ba25f5108092bff43a4652df07bd296
43068048877 <mayaracaetano619@gmail.com>
2026-08-11 09:44:51 -0300

    nova atividade

---
+++ funcoes/exercicios/exercico_10.py
@@ -0,0 +1,9 @@
+def temperatura():
+    celsius = int(input("Digite a temperatura em Celsius: "))
+
+    fahrenheit = celsius * 1.8 + 32
+
+    print(f"A temperatura em Fahrenheit é: {fahrenheit:}°F")
+
+
+temperatura()
