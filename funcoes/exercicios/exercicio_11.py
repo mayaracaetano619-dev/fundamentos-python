00682d6af110ea41b7c39a7a43fca890085e4fe7
43068048877 <mayaracaetano619@gmail.com>
2026-08-11 09:55:31 -0300

    nova atividade

---
+++ funcoes/exercicios/exercicio_11.py
@@ -0,0 +1,10 @@
+def salario():
+    valor_hora = int(input('Digite o valor da hora trabalhada: '))
+    horas = int(input('Digite a quantidade de horas trabalhadas: '))
+
+    salario = valor_hora * horas
+
+    print(f'O salário é: R$ {salario:}')
+
+
+salario()
