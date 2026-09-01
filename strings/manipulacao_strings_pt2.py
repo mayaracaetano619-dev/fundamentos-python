363778fa01806278e5b7b3b12fdf751123b81312
43068048877 <mayaracaetano619@gmail.com>
2026-09-01 13:11:22 -0300

    nova atividade

--- strings/manipulacao_strings_pt2.py
+++ strings/manipulacao_strings_pt2.py
@@ -13,3 +13,37 @@ def criar_nome_completo(partes):
 
 partes_nome = ['Mayara', 'Caetano']
 print(f'Nome completo: {criar_nome_completo(partes_nome)}')
+
+# Verificar o início e o final de uma string
+def analizar_url(url):
+    com_https = url.startswith('https://')
+    termina_com_br = url.endswith('.br')
+    return com_https, termina_com_br
+
+url = 'https://www.gov.br'
+tem_https, tem_br = analizar_url(url)
+print(f'Utiliza https?: {tem_https}')
+print(f'Termina com br?: {tem_br}')
+
+
+# Verificar se a string contem somento numeros
+def validar_idade(idade):
+    idade_valida = idade.isdigit()
+    if idade_valida:
+        print('O valor digitado é uma idade válida')
+    else:
+        print('Digite somente numeros!')
+
+idade = input('Digite sua idade: ')
+validar_idade(idade)
+
+# Verificar se a string contem somento letras
+def validar_nome(nome):
+    nome_valido = nome.isalpha()
+    if nome_valido:
+        print('Nome válido')
+    else:
+        print('Digite somente letras!')
+
+nome = input('Digite um nome válido: ')
+validar_nome(nome)
